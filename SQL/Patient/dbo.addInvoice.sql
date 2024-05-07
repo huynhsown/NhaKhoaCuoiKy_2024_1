7e961e0ceecf01afab752a0dfa4085ed6e4ea60c
jayson <sondanghuynh@gmail.com>
2024-05-07 21:24:58 +0700

    addInvoice

---
+++ SQL/Patient/dbo.addInvoice.sql
@@ -0,0 +1,14 @@
+﻿CREATE PROCEDURE addInvoice
+    @MaBenhNhan INT,
+    @NgayThamKham DATE,
+	@MaHoaDon INT OUTPUT
+AS
+BEGIN
+
+    INSERT INTO HOADON (MaBenhNhan, NgayThamKham)
+    VALUES (@MaBenhNhan, @NgayThamKham);
+
+    SET @MaHoaDon = SCOPE_IDENTITY();
+
+	SELECT @MaHoaDon AS MaHoaDon;
+END;
